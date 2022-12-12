036083fa50af42f7a9107cf6e86fbfd466f6bfcd
Laishyde <laiscristina.animamundi@gmail.com>
2022-12-11 22:55:51 -0300

    iniciando

--- retro-game/retro-game/src/app/app.module.ts
+++ retro-game/retro-game/src/app/app.module.ts
@@ -2,13 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { LandingPageComponent } from './module/landing-page/landing-page/landing-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingPageComponent
   ],
   imports: [
     BrowserModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
