cf6ce1376eb5e15b8df81d76b84887d8ab474149
Laishyde <laiscristina.animamundi@gmail.com>
2023-02-04 19:03:32 -0300

    projeto concluído adicionado imagens e javascrpit para a aplicação

---
+++ Aprendendo-lógica/funcao.js
@@ -0,0 +1,11 @@
+var nome = prompt("Olá qual seu nome");
+alert("Ben vindo" + nome);
+
+var idade = 18;
+var idade = prompt("digite sua idade");
+if (idade <= 18){
+    alert("voce é de menor");
+ 
+}else( idade =>20)
+    alert("voce é maior");
+
